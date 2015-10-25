b801056f971a91beab91673c5c8e307e80824451
anson17 <ansonl@users.noreply.github.com>
2015-10-25 09:24:38 -0400

    read all emails in emails file

--- script.sh
+++ script.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-while read line; do
-  echo "$line read"
-done < emails
-
 randomImages[0]="http:\\/\\/i.imgur.com\\/eT1Tuws.jpg"
-randomImages[1]="http:\\/\\/i.imgur.com\\/BdBLFp6.gif"
-randomImages[2]="http:\\/\\/i.imgur.com\\/aVwNMEx.gif"
+randomImages[1]="http:\\/\\/i.imgur.com\\/dacU3tc.png"
+randomImages[2]="http:\\/\\/i.imgur.com\\/t7ZxaBD.jpg"
 randomImages[3]="http:\\/\\/i.imgur.com\\/52WMnWV.png"
 randomImages[4]="http:\\/\\/i.imgur.com\\/vaOxaJ1.jpg"
-randomImageURL=${randomImages[$(($RANDOM % 5))]}
-
-from="cybergod@usa"
-to="m163876@usna.edu"
-subject="Halloween gram confirmation"
-sed 's/NAME//' htmlMessage.html > tmp
-message=$(sed 's/RANDOMIMAGE/'$randomImageURL'/' tmp)
+randomImages[5]="http:\\/\\/i.imgur.com\\/6X9P0sZ.png"
+randomImages[6]="http:\\/\\/i.imgur.com\\/0j50DRS.png"
+randomImages[7]="http:\\/\\/i.imgur.com\\/5gpuHly.png"
+randomImages[8]="http:\\/\\/i.imgur.com\\/PE3FtgK.png"
 
 
-(
-echo "From: $from";
-echo "To: $to";
-echo "Reply-to: m171500@usna.edu";
-echo "Reply-to: m163876@usna.edu";
-echo "Subject: $subject";
-echo "Content-Type: text/html"
-echo "MIME-Version: 1.0";
-echo "";
-echo "$message";
-) | sendmail -t
+sendMail()
+{
+    randomImageURL=${randomImages[$(($RANDOM % 9))]}
+
+    from="cybergod@gonavy"
+    to=$currentRecipient
+    subject="Halloween gram confirmation"
+    sed 's/NAME//' htmlMessage.html > tmp
+    message=$(sed 's/RANDOMIMAGE/'$randomImageURL'/' tmp)
+
+    (
+    echo "From: $from";
+    echo "To: $to";
+    echo "Reply-to: m171500@usna.edu";
+    echo "Reply-to: m163876@usna.edu";
+    echo "Subject: $subject";
+    echo "Content-Type: text/html"
+    echo "MIME-Version: 1.0";
+    echo "";
+    echo "$message";
+    ) | sendmail -t
+}
+
+
+while read line; do
+    currentRecipient=$line
+    sendMail
+    count=$((count+1))
+    echo "Sent confirmation to $currentRecipient"
+done < emails
+
+echo "$count messages sent."
